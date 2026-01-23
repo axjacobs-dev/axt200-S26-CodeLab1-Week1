249e71aad91d9b8d88955544109bc2bc19a66bd9
axjacobs-dev <axjacobs@gmail.com>
2026-01-23 16:42:16 -0500

    added players and updated controller script

--- CodeLab-S25-Week1/Assets/Scripts/WASDController.cs
+++ CodeLab-S25-Week1/Assets/Scripts/WASDController.cs
@@ -3,14 +3,17 @@ using UnityEngine.InputSystem;
     
 public class WASDController : MonoBehaviour
 { 
-    Rigidbody rb;
+    Rigidbody rb; //rigidbody for the GameObject that this script is attached to
 
-    public float moveForce = 10f;
+    public float moveForce = 10f; //the force we add to a GameObject to make it move
 
-    public Key keyUp = Key.W;
+    public Key keyUp = Key.W; //the key we press to make GameObject go up for use with new input system
+
+    public Key keyDown = Key.S; //the key we press to make GameObject go down for use in new input system
 
     Keyboard keyboard = Keyboard.current;
-    
+
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -21,13 +24,21 @@ public class WASDController : MonoBehaviour
 
         if (keyboard[keyUp].isPressed)
         {
-            rb.AddForce(Vector3.up * moveForce, ForceMode.Acceleration);
+            rb.AddForce(Vector3.up * moveForce, ForceMode.Acceleration); //give GameObject an upward force
+            Debug.Log ("going up!");
         }
+
+        if (keyboard[keyDown].isPressed)
+        {
+            rb.AddForce(Vector3.down* moveForce, ForceMode.Acceleration); //give GameObject a downward force
+            Debug.Log ("going down!");
+        }
+
         
         
         // if(Input.GetKey(KeyCode.S))
         // {
-        //     rb.AddForce(Vector3.down * moveForce, ForceMode.Acceleration);
+        //     rb.AddForce(Vector3.down * moveForce, ForceMode.Acceleration); // give the GameObject a downward force
         // }
     }
 }
