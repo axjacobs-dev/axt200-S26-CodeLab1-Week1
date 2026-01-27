0aa6f6c86b38ccce52d6233073c5a1332521e6cd
axjacobs-dev <axjacobs@gmail.com>
2026-01-27 16:58:55 -0500

    updated player controller script

--- CodeLab-S26-Week1/Assets/Scripts/PlayerController.cs
+++ CodeLab-S26-Week1/Assets/Scripts/PlayerController.cs
@@ -11,9 +11,17 @@ public class PlayerController : MonoBehaviour
 
     public InputActionAsset inputActions;
 
+    //public PlayerInput Player;
+
     private InputAction moveAction;
     
     private InputAction jumpAction;
+    
+    public float horizontalInput; 
+    
+    public float verticalInput;
+    
+    public Vector2 moveInput;
     
     //need to add horizontal and vertical input and call it in the moveAction method
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -37,32 +45,49 @@ public class PlayerController : MonoBehaviour
     {
         moveAction = inputActions.FindAction("Move");
         jumpAction = inputActions.FindAction("Jump");
+        moveInput = inputActions.Player.Move.ReadValue<Vector2>();
+        horizontalInput = moveInput.x;
+        verticalInput = moveInput.y;
     }
 
     // Update is called once per frame
     private void FixedUpdate()
     {
+        Move();
+        Jump();
+    }
+
+    private void Jump()
+    {
         if (jumpAction.IsPressed())
         {
             rb.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
         }
+    }
 
+    private void Move()
+    {
         if (moveAction.IsPressed())
         {
-            if (keyboard[keyLeft].isPressed)
-            {
-                //rb.AddForce(Vector3.left * moveForce, ForceMode.Acceleration); //give GameObject a leftward force
-                transform.position += Vector3.left * Time.deltaTime;
-                Debug.Log ("going left!");
-            }
 
-            if (keyboard[keyRight].isPressed)
-            {
-                //rb.AddForce(Vector3.right * moveForce, ForceMode.Acceleration); //give GameObject a rightward force
-                //rb.linearVelocity = transform.right * moveSpeed;
-                transform.position += Vector3.right * Time.deltaTime;
-                Debug.Log ("going right!");
-            }
         }
     }
+
+    // {
+    //         if (keyboard[keyLeft].isPressed)
+    //         {
+    //             //rb.AddForce(Vector3.left * moveForce, ForceMode.Acceleration); //give GameObject a leftward force
+    //             transform.position += Vector3.left * Time.deltaTime;
+    //             Debug.Log ("going left!");
+    //         }
+    //
+    //         if (keyboard[keyRight].isPressed)
+    //         {
+    //             //rb.AddForce(Vector3.right * moveForce, ForceMode.Acceleration); //give GameObject a rightward force
+    //             //rb.linearVelocity = transform.right * moveSpeed;
+    //             transform.position += Vector3.right * Time.deltaTime;
+    //             Debug.Log ("going right!");
+    //         }
+    //     }
+    // }
 }
